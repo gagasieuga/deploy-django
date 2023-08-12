f71be5c82e7769a154497a1dd989c0a66110cd89
Ph. Nam Anh <na.ph0304@gmail.com>
2023-08-12 09:41:13 +0700

    static root changed

--- DjangoWebsite/settings.py
+++ DjangoWebsite/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # the path is DjangoWebsite\static
 
 MEDIA_URL = 'media/' #the path for media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # the path is DjangoWebsite\media
